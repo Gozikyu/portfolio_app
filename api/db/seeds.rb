d1cfbd454113d617d8cdbb8345ee13aac6462a88
Yuta Uemura <Yuta@koukadai-no-MacBook-Pro-74.local>
2021-01-11 07:31:48 +0900

    install rubocop

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name: 'Example User',
+             email: 'example@railstutorial.org',
              password: 'password',
              password_confirmation: 'password')
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
+  email = "example-#{n + 1}@railstutorial.org"
+  password = 'password'
+  User.create!(name: name,
                email: email,
-               password:              password,
+               password: password,
                password_confirmation: password)
 end
-
