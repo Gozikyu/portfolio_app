ff6e0cdaddbf84b617d214dc12a11fbd8ce0d21f
Yuta Uemura <Yuta@koukadai-no-MacBook-Pro-74.local>
2021-01-17 15:57:11 +0900

    edit rspec

--- api/spec/requests/sessions_request_spec.rb
+++ api/spec/requests/sessions_request_spec.rb
@@ -22,10 +22,12 @@ RSpec.describe "Sessions", type: :request do
       expect(response).to have_http_status(404)
     end
 
-    it 'remember user by session' do
+    it 'remember and forget user_id by session ' do
       FactoryBot.create(:user)
       post '/login', params: { user: {email: 'hoge@gmail.com', password: 'password'} } 
       expect(session[:user_id].nil?).to be false
+      delete '/logout'
+      expect(session[:user_id].nil?).to be true
     end 
   end
 end
